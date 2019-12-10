a22cb6986151192d6b847a315b156337343319e4
Niggel <katerdudel@gmail.cp>
2019-12-10 20:59:11 +0100

    Scrolling changed

--- app/todoapp/lib/UI/Intray/intray_page.dart
+++ app/todoapp/lib/UI/Intray/intray_page.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todoapp/models/global.dart';
@@ -50,7 +52,9 @@ class _IntrayPageState extends State<IntrayPage> {
 
                     if(snapshot.hasData){
                       return WebView(
-                      
+                        gestureRecognizers: [
+        Factory(() => PlatformViewVerticalGestureRecognizer()),
+      ].toSet(),
                         //initialUrl: "https://pub.dev/packages/webview_flutter#-installing-tab-",
 
                         
@@ -192,4 +196,44 @@ class _IntrayPageState extends State<IntrayPage> {
 
     return apiKey;
   }
+
+}
+
+
+class PlatformViewVerticalGestureRecognizer
+    extends VerticalDragGestureRecognizer {
+  PlatformViewVerticalGestureRecognizer({PointerDeviceKind kind})
+      : super(kind: kind);
+
+  Offset _dragDistance = Offset.zero;
+
+  @override
+  void addPointer(PointerEvent event) {
+    startTrackingPointer(event.pointer);
+  }
+
+  @override
+  void handleEvent(PointerEvent event) {
+    _dragDistance = _dragDistance + event.delta;
+    if (event is PointerMoveEvent) {
+      final double dy = _dragDistance.dy.abs();
+      final double dx = _dragDistance.dx.abs();
+
+      if (dy > dx && dy > kTouchSlop) {
+        // vertical drag - accept
+        resolve(GestureDisposition.accepted);
+        _dragDistance = Offset.zero;
+      } else if (dx > kTouchSlop && dx > dy) {
+        // horizontal drag - stop tracking
+        stopTrackingPointer(event.pointer);
+        _dragDistance = Offset.zero;
+      }
+    }
+  }
+
+  @override
+  String get debugDescription => 'horizontal drag (platform view)';
+
+  @override
+  void didStopTrackingLastPointer(int pointer) {}
 }
